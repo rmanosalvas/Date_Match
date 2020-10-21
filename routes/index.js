5f65a019b824e48a0ac5959a70a2966ab400ac46
Ricardo <manosalvas2013@gmail.com>
2020-10-20 22:08:41 -0400

    james

--- routes/index.js
+++ routes/index.js
@@ -5,24 +5,30 @@ const apiRoutes = require("./api");
 const passport = require("../config/passport"); // Requiring passport
 const isAuthenticated = require("../config/middleware/isAuthenticated.js");//Checks that a user has been authenticated
 
+// dashboard route 
+router.get('/', isAuthenticated, function (req, res) {
+  // if the user is authenticated redirect to dashboard
+  res.redirect('/dashboard');
+});
+
 // LOGIN route
 router.post('/api/login', 
-  passport.authenticate('local', { failureRedirect: '/signup' }),
+  passport.authenticate('local', { successRedirect: '/dashboard', failureRedirect: '/login', failureFlash : true  }),
   function(req, res) {
     console.log(req)
     // redirect the user to the dashboard
-    res.redirect('/dashboard');
+    
     console.log("****************************")
     console.log(res)
   });
 
 // API Routes
-router.use("/api", isAuthenticated, apiRoutes );
+router.use("/api", apiRoutes );
 
-// HTML routes
+// dashboard route 
 router.get('/dashboard', isAuthenticated, function (req, res) {
   // dashboard
-  res.redirect('/password');
+  res.redirect('/dashboard');
 });
 
 router.get("/logout", function (req, res) {
